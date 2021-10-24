3437ee331efb775335a5e866895df7aa5899bf4b
Yu-Teng Li <yutengli@berkeley.edu>
2021-10-24 16:03:48 -0700

    Code for demo submited (to be edited further)

--- flutter_app/app/lib/main.dart
+++ flutter_app/app/lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:ffi';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -42,17 +49,26 @@ class _DemoDashboardState extends State<DemoDashboard> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              height: 90,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Image.network(
+                    'https://cdn-icons.flaticon.com/png/512/2592/premium/2592258.png?token=exp=1635113913~hmac=a15ccb9d49aeea0f50fbb48ccd47e847',
+                    height: 60),
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(_demoOne());
               },
-              child: const Text('Demo 1!'),
+              child: const Text('Face Tracking Demo'),
             ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).push(_demoTwo());
                 },
-                child: const Text('Demo 2!'))
+                child: const Text('Tumor Detection Demo'))
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -103,8 +119,15 @@ class Demo1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Face Tracking")),
-      body: const Center(
-        child: Text('Demo 1'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Upload a video",
+                style: const TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(height: 180, child: ImagePick())
+          ],
+        ),
       ),
     );
   }
@@ -119,6 +142,104 @@ class Demo2 extends StatelessWidget {
       appBar: AppBar(title: const Text("Tumor Detection")),
       body: const Center(
         child: Text('Demo 2'),
+      ),
+    );
+  }
+}
+// >>>>>>>>>>>> Image Picker section >>>>>>>>>>>
+
+class ImagePick extends StatefulWidget {
+  @override
+  _ImagePickState createState() => _ImagePickState();
+}
+
+class _ImagePickState extends State<ImagePick> {
+  File? _image;
+
+  Future getImagefromGallery() async {
+    final picker = ImagePicker();
+    var image = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 120.0,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                  height: 95,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 85.0,
+                    child: Center(
+                      child: _image == null
+                          ? Text("No videos selected")
+                          : Image.file(_image as File),
+                    ),
+                  )),
+            ),
+          ),
+          SizedBox(
+            child: FloatingActionButton(
+              onPressed: getImagefromGallery,
+              tooltip: "Pick Image",
+              child: _image == null
+                  ? Icon(Icons.camera_alt)
+                  : ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(_resultOne());
+                      },
+                      child: Text("Go!"),
+                    ),
+            ),
+            width: 60,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Route _resultOne() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const Result1(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class Result1 extends StatelessWidget {
+  const Result1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Inference"), backgroundColor: Colors.blue[200]),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[Text("Result sent to server for inference.")],
+        ),
       ),
     );
   }
